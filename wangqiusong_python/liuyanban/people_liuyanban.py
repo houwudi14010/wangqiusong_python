40d570eea185efc01fa83fa3b9bc5ec45a48a6a5
wqs14010 <948700696@qq.com>
2022-10-18 11:48:28 +0800

    启动任务添加

--- wangqiusong_python/liuyanban/people_liuyanban.py
+++ wangqiusong_python/liuyanban/people_liuyanban.py
@@ -201,7 +201,7 @@ class people_liuyanban(feapder.Spider):
                   '1459',
                   '1460',
                   '1461', '1462', '1463', '1464', '600', '601']
-        fidArr = [539]
+        # fidArr = [539]
         for fids in fidArr:
             for nums in arr:
                 type = 2
